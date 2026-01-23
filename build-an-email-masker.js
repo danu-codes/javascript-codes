a36a4f60b5fb6d72ea8dfaa3d486927adb7c456f
Sri_Danushan <mailtodanu07@gmail.com>
2026-01-23 21:46:06 +0530

    Add files via upload

---
+++ build-an-email-masker.js
@@ -0,0 +1,32 @@
+** start of script.js **
+
+function maskEmail(email) {
+ 
+  const atIndex = email.indexOf('@');
+  
+  
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+  
+  
+  if (username.length <= 2) {
+    return username + domain;
+  }
+  
+  
+  const masked = username[0] + '*'.repeat(username.length - 2) + username[username.length - 1];
+  
+  
+  return masked + domain;
+}
+
+let email = "apple.pie@example.com";
+console.log(maskEmail(email)); 
+
+console.log(maskEmail("freecodecamp@example.com")); 
+console.log(maskEmail("info@test.dev"));            
+console.log(maskEmail("user@domain.org"));         
+
+
+** end of script.js **
+
